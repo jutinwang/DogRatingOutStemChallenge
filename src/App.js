83f539742993a880f2dae13a3ef6f86fab13a3ea
justinwang <justinwang407@gmail.com>
2023-01-18 18:24:12 -0500

    Added filtering by breed

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import { useState, useEffect } from 'react';
 import { Rating } from 'react-simple-star-rating';
 import './App.css';
 
-const apilink = "https://dog.ceo/api/breeds/image/random"
+const randomDogLink = "https://dog.ceo/api/breeds/image/random"
+const allBreeds = "https://dog.ceo/api/breeds/list/all"
 
 function App() {
+  var clicked = false;
+  // let dogViewed = [];
 
   // https://www.pluralsight.com/guides/access-data-from-an-external-api-into-a-react-component
   // API to show random dog
@@ -15,12 +18,47 @@ function App() {
   }, []);
 
   const fetchDogLink = async() =>{
-    let response = await fetch(apilink);
+    let response = null;
+    if (breedChosen != "Select a Dog Breed"){
+      response = await fetch(`https://dog.ceo/api/breed/${breedChosen}/images/random`);
+    }else{
+      response = await fetch(randomDogLink)
+    }
     var data = await response.json();
+
+    var manipulateList = document.getElementById("breeds")
     console.log(data.message);
     setDogData(data);
-    setRating(10)
+    setRating(10);
+    getBreeds(manipulateList.value)
+    console.log(manipulateList.value);
+    // dogViewed.push({image: data.message, rating: rating})
+    // console.log(dogViewed)
   }
+
+  const [breedChosen, getBreeds] = useState(0);
+
+  useEffect(() => {
+    createDogList();
+  }, []);
+
+  const createDogList = async() => {
+    let listResponse = await fetch(allBreeds);
+    var data = await listResponse.json();
+    var arrayOfNames = Object.keys(data.message)
+
+    var manipulateList = document.getElementById("breeds");
+
+    for (let i = 0; i < arrayOfNames.length; i++) {
+      var opt = document.createElement("option");
+      manipulateList.innerHTML += '<option>' + arrayOfNames[i] + '</option>';
+    }
+    
+    getBreeds(manipulateList.value);
+    // console.log(manipulateList.value);
+  }
+  
+
 
   // https://www.npmjs.com/package/react-simple-star-rating?activeTab=readme
   // Star Rating stuff
@@ -29,33 +67,62 @@ function App() {
   // Catch Rating value
   const handleRating = (rate: number) => {
     setRating(rate + 10)
+    clicked = true
 
     // other logic
   }
 
   // Optinal callback functions
-  const onPointerEnter = () => console.log('Enter')
-  const onPointerLeave = () => console.log('Leave')
-  const onPointerMove = (value: number, index: number) => console.log(value, index)
+  const onPointerEnter = () => {
+    console.log('Enter')
+  }
+  const onPointerLeave = () => {
+    console.log('Leave')
+    if (clicked == false){
+      setRating(10)
+    }
+  }
+  const onPointerMove = (value: number, index: number) => {
+    console.log(value, index)
+    setRating(value + 10)
+  }
 
   //Render onto React
   return (
     <div className="App">
-      <h1>Title</h1>
+      <h1>Rate My Dog!</h1>
       <div className='DogImageFrame'>
         <img className='DogImage' src={dogImage.message}></img>
       </div>
+
+      <div className="BreedSelecter">
+        <select id = "breeds">
+          <option>Select a Dog Breed</option>
+        </select>
+      </div>
+
       <Rating
         onClick={handleRating}
         onPointerEnter={onPointerEnter}
         onPointerLeave={onPointerLeave}
-        onPointerMove={onPointerMove}
-        
+        onPointerMove={onPointerMove} 
       />
+
       <p>{rating} / 10</p>
       <div className='button'>
         <button onClick={fetchDogLink}>Click for New Dog</button>
       </div>
+
+      {/* <table>
+        <thead>
+        <tr>
+          <th>Image</th>
+          <th>Rating</th>
+        </tr>
+        </thead>
+      </table> */}
+
+
     </div>
   );
 }
